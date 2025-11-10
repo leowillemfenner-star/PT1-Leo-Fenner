d91861a040ced9a258f8f2b4adee920e5cc0bd8e
Leo Fenner <leowillemfenner@gmail.com>
2025-11-10 11:05:05 +0100

    Refactor password validation and strength calculation

--- passwords.c
+++ passwords.c
@@ -5,7 +5,6 @@
 
 bool passwordIsPrintable(char password[])
 {
-    // Alle druckbaren ASCII-Zeichen liegen zwischen 32 und 126
     for (int i = 0; i < strlen(password); i++)
     {
         if (password[i] < 32 || password[i] > 126)
@@ -18,15 +17,13 @@ bool passwordIsPrintable(char password[])
 
 int main()
 {
-    char password[43]; // Maximal 42 Zeichen + Nullterminator
+    char password[43]; 
     printf("Bitte geben Sie ein Passwort ein:\n");
     if (scanf("%42s", password) != 1)
     {
         fprintf(stderr, "Fehler beim Einlesen des Passworts.\n");
         return 1;
     }
-
-    // Prüfen, ob alle Zeichen druckbar sind
     if (!passwordIsPrintable(password))
     {
         fprintf(stderr, "Fehler: Passwort enthält undruckbare Zeichen.\n");
@@ -34,8 +31,6 @@ int main()
     }
 
     int length = strlen(password);
-
-    // Ermitteln, welche Zeichengruppen vorkommen
     bool hasLower = false, hasUpper = false, hasDigit = false, hasSpecial = false;
     for (int i = 0; i < length; i++)
     {
@@ -44,24 +39,16 @@ int main()
         else if (password[i] >= '0' && password[i] <= '9') hasDigit = true;
         else hasSpecial = true;
     }
-
-    // Summe der Zeichenanzahlen der genutzten Gruppen
     int charset = 0;
     if (hasLower) charset += 26;
     if (hasUpper) charset += 26;
     if (hasDigit) charset += 10;
     if (hasSpecial) charset += 33;
-
-    // Entropie berechnen
     double entropy = length * log2(charset);
-
-    // Stärke einschätzen
     char *strength;
     if (entropy < 40) strength = "schwach";
     else if (entropy >= 80) strength = "stark";
     else strength = "mittel";
-
-    // Ergebnis ausgeben
     printf("Entropie: %.1f\n", entropy);
     printf("Stärke: %s\n", strength);
 
